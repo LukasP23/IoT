a7898278886c738e1bdda77c11d242998c6598fa
LukasP23 <40456378@live.napier.ac.uk>
2021-11-15 22:33:32 +0000

    Connect Database

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
 
 app.use(express.static("public"));
+
+const mysql = require("mysql");
+const fs = require("fs");
+var con = mysql.createConnection({
+  host: "34.123.88.230",
+  user: "root",
+  password: "Password123",
+  database: "sensorDatabase",
+  ssl: {
+    ca: fs.readFileSync(__dirname + "/dbKeys/server-ca.pem"),
+    key: fs.readFileSync(__dirname + "/dbKeys/client-key.pem"),
+    cert: fs.readFileSync(__dirname + "/dbKeys/client-cert.pem"),
+  },
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+});
+
+app.get("/location", (request, response) => {
+  con.query(
+    "SELECT * FROM data \n " +
+      "Limit 1 ",
+
+    function (err, result, fields) {
+      if (err) throw err;
+
+      const latitude = result[0].latitude;
+      const longitude = result[0].longitude;
+
+      response.json({ latitude, longitude });
+    }
+  );
+});
